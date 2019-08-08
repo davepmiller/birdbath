82a55191c192f46fe8b7bcc79fb083b78272c68f
Dave Miller <dpmdavemiller@gmail.com>
2019-08-07 20:22:33 -0600

    Placeholder commit

--- tweet-wrangler.py
+++ tweet-wrangler.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import tweepy
 import yaml
+import json
 
 def parse_config(config_path):
   with open(config_path, 'r') as ymlfile:
@@ -9,10 +10,13 @@ def parse_config(config_path):
     except yaml.YAMLError as e:
       print(e)
 
-config = parse_config('./.get-tweets.yml')
+config = parse_config('../.get-tweets.yml')
 auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
 auth.set_access_token(config['access_token'], config['access_token_secret'])
 api = tweepy.API(auth)
 tweets = api.user_timeline(screen_name = config['screen_name'])
 for tweet in tweets:
+    print(tweet._json['id'])
+    print(tweet._json['created_at'])
     print(tweet)
+    break
